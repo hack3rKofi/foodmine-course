3fdccfbf062ac16f5075bfb16e1bd255c633a6f0
Kofi Boateng <hack3rkofi@gmail.com>
2023-09-30 22:16:26 +0000

    Create the Header Component

---
+++ README.md
@@ -0,0 +1,12 @@
+## lessons
+1. Introduction to the course
+2. Install Development tools => Angular Essentials Extension
+3. Create Angular App
+    1. Create project's folder
+    2. Install @angular/cli
+    3. Create App as frontend
+
+4. Add Header
+    1. Generate Component
+    2. Add Html
+    3. Add Css
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/partials/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([])
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
